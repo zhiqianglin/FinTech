e3c2cf5efa1f1a894869cc331ab3965785f46294
Ann Li <f1annlee@lawn-143-215-62-98.lawn.gatech.edu>
2017-02-12 03:35:38 -0500

    added data files

--- test_ann.py
+++ test_ann.py
@@ -7,6 +7,19 @@ This is a temporary script file.
 
 print('hello world')
 
-!git add test_ann.py
-!git commit -m “added something”
+print('haha')
+"""
+!git pull
+!git add test_ann.py # this step can be ignored
+!git commit -m "add"
 !git push -u origin master
+"""
+import pandas as pd
+df_th=pd.read_csv('transactionhistory.csv')
+
+# extract recent transactions
+df_th_recent=df_th[df_th['date']>20161001]
+# extract dining transactions
+df_th_recent_dining=df_th_recent[df_th_recent['eventCategory']==1]
+# personal transaction summary
+df_summary=df_th_recent_dining[['amount','accountId','merchant','postalCode']].groupby(['accountId','merchant','postalCode']).agg(['sum', 'count'])
